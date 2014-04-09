4889ea8d5fda744ecd4a3adce72204e816c94f0a
Fred Trovão <fredjampa@gmail.com>
2014-04-09 18:05:33 -0300

    ...

--- src/main/java/br/ufpb/tcc/Insere.java
+++ src/main/java/br/ufpb/tcc/Insere.java
@@ -18,7 +18,7 @@ public class Insere {
 	public static void main(String[] args) throws TccException {
 		System.out.println("Processando...");
 		long inicio = System.currentTimeMillis();
-		createBase(1000000, 2, 20, 2, Bancos.CASSANDRA.ordinal());
+		createBase(1000, 2, 5, 2, Bancos.CASSANDRA.ordinal());
 		
 		long fim = System.currentTimeMillis();
 		
